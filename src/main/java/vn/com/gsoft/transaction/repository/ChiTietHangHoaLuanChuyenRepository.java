276a60d791b3e2d0f461f329389113f6dd419791
vuductinh <49410355+vuductinh@users.noreply.github.com>
2024-09-21 11:30:23 +0700

    Update ChiTietHangHoaLuanChuyenRepository.java

--- src/main/java/vn/com/gsoft/transaction/repository/ChiTietHangHoaLuanChuyenRepository.java
+++ src/main/java/vn/com/gsoft/transaction/repository/ChiTietHangHoaLuanChuyenRepository.java
@@ -46,7 +46,7 @@ public interface ChiTietHangHoaLuanChuyenRepository extends BaseRepository<ChiTi
             + " AND ((:#{#param.thuocId} IS NULL) OR (c.thuocId = :#{#param.thuocId})) "
             + " AND ((:#{#param.maGiaoDich} IS NULL) OR (c.maGiaoDich = :#{#param.maGiaoDich})) "
             + " AND ((:#{#param.trangThai} IS NULL) OR (c.trangThai = :#{#param.trangThai})) "
-            + " AND (c.maCoSoGui = :#{#param.maCoSoGui} AND (c.trangThai in (:#{#param.trangThai1s})))" +
+            + " AND ((c.maCoSoGui = :#{#param.maCoSoGui} AND (c.trangThai in (:#{#param.trangThai1s})))" +
             " OR (c.maCoSoNhan = :#{#param.maCoSoGui} AND (c.trangThai in (:#{#param.trangThais})))) "
             + " AND (:#{#param.fromDate} IS NULL OR c.created >= :#{#param.fromDate}) "
             + " AND (:#{#param.toDate} IS NULL OR c.created <= :#{#param.toDate})" +
